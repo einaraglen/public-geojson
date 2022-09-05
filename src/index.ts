b17442aca31238cd50a02b9f05d3a9a6a6484c3e
Einar Aglen <einar.aglen99@gmail.com>
2022-09-05 08:46:28 +0200

    Added dotenv for port reading in deployment environment

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import { version } from '../package.json'
 import { normal } from './tools/normal'
 import { pipelines } from './tools/pipelines'
 import { turbines } from './tools/turbines'
+import dotenv from "dotenv"
+
+dotenv.config()
+
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req: any, res: any) => res.send(`Public Geo JSON v${version}`))
 
@@ -16,6 +20,6 @@ app.get('/terminals', (req: any, res: any) => normal('terminals.csv').then((data
 
 app.get('/extract', (req: any, res: any) => normal('extract.csv').then((data) => res.send(data)))
 
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`public-geosjon@${version} - listening at PORT ${process.env.PORT || PORT}`)
+app.listen(PORT, () => {
+  console.log(`public-geosjon@${version} - listening at PORT ${PORT}`)
 })
