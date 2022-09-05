9f635d5656102ca0d6fb93b5146520d6caf83734
Einar Aglen <einar.aglen99@gmail.com>
2022-09-05 08:59:02 +0200

    Added routes to "/"

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,32 @@
-import express from 'express'
-import { version } from '../package.json'
-import { normal } from './tools/normal'
-import { pipelines } from './tools/pipelines'
-import { turbines } from './tools/turbines'
-import dotenv from "dotenv"
+import express from "express";
+import { version } from "../package.json";
+import { normal } from "./tools/normal";
+import { pipelines } from "./tools/pipelines";
+import { turbines } from "./tools/turbines";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req: any, res: any) => res.send(`Public Geo JSON v${version}`))
+const routes = ["pipelines", "turbines", "terminals", "extracts"];
 
-app.get('/pipelines', (req: any, res: any) => pipelines().then((data) => res.send(data)))
+app.get("/", (req: any, res: any) =>
+  res.send({
+    message: `Public Geo JSON v${version}`,
+    routes,
+  })
+);
 
-app.get('/turbines', (req: any, res: any) => turbines().then((data) => res.send(data)))
+app.get("/pipelines", (req: any, res: any) => pipelines().then((data) => res.send(data)));
 
-app.get('/terminals', (req: any, res: any) => normal('terminals.csv').then((data) => res.send(data)))
+app.get("/turbines", (req: any, res: any) => turbines().then((data) => res.send(data)));
 
-app.get('/extract', (req: any, res: any) => normal('extract.csv').then((data) => res.send(data)))
+app.get("/terminals", (req: any, res: any) => normal("terminals.csv").then((data) => res.send(data)));
+
+app.get("/extracts", (req: any, res: any) => normal("extract.csv").then((data) => res.send(data)));
 
 app.listen(PORT, () => {
-  console.log(`public-geosjon@${version} - listening at PORT ${PORT}`)
-})
+  console.log(`public-geosjon@${version} - listening at PORT ${PORT}`);
+});
